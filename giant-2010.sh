6a71392e3bdd5691e792e35c41808365441f5099
Dung Le <xdung24@gmail.com>
2022-11-29 20:10:13 +0700

    update script

--- giant-2010.sh
+++ giant-2010.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-folder="${PWD}/subtitle/Giant"
+folder="${PWD}/movies/giant-2010"
 infile="${folder}/in.txt"
 echo $folder
 outfile="${folder}/out.txt"
@@ -10,18 +10,16 @@ deslang=vi
 
 i=0
 j=1
-. /home/nuc/Documents/subtranslate/venv/bin/activate
+. ./subtranslate/venv/bin/activate
 readarray -t outarr < $outfile
 cat $infile | while read line 
 do
   echo '['$j']' $line
   src='"'$folder/$srclang/$line'"'
   des='"'$folder/$deslang/${outarr[${i}]}'"'
-  echo $src
-  echo $des
-  cmd="python3 /home/nuc/Documents/subtranslate/main.py --src-lang=$srclang --des-lang=$deslang --input-srt=$src --output-srt=$des"
+  cmd="python3 ./subtranslate/main.py --src-lang=$srclang --des-lang=$deslang --input-srt=$src --output-srt=$des"
   echo $cmd
-  eval $cmd
+  #eval $cmd
   i=$((i+1))
   j=$((j+1))
 done
