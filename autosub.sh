9bc15eaf039a17c2d241e237e3f1ea03a84da748
Dung Le <xdung24@gmail.com>
2022-11-29 20:57:34 +0700

    skip done files

--- autosub.sh
+++ autosub.sh
@@ -4,21 +4,29 @@
 folder=${PWD}/$1
 infile="${folder}/in.txt"
 outfile="${folder}/out.txt"
+donefile="${folder}/done.txt"
 
 srclang=en
 deslang=vi
 
 i=0
-. ./subtranslate/venv/bin/activate
+source ${PWD}/subtranslate/venv/bin/activate
 readarray -t outarr < $outfile
 cat $infile | while read line 
 do
   echo '['$((i+1))']' $line
+  name=${outarr[${i}]}
   src='"'$folder$srclang/$line'"'
-  des='"'$folder$deslang/${outarr[${i}]}'"'
+  des='"'$folder$deslang/${name}'"'
   cmd="python3 ./subtranslate/main.py --src-lang=$srclang --des-lang=$deslang --input-srt=$src --output-srt=$des"
-  echo $cmd
-  eval $cmd
+  if grep -Fx "$name" $donefile
+  then
+    echo 'already done, skip'
+  else
+    echo $cmd
+    eval $cmd
+    echo $name>>$donefile
+  fi
   i=$((i+1))
 done
 
