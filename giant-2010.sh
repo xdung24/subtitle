9db427153ccd09a68a14f29d10e1cc40e05d2514
Dung Le <xdung24@gmail.com>
2022-11-29 20:11:36 +0700

    Update giant-2010.sh

--- giant-2010.sh
+++ giant-2010.sh
@@ -9,18 +9,16 @@ srclang=en
 deslang=vi
 
 i=0
-j=1
 . ./subtranslate/venv/bin/activate
 readarray -t outarr < $outfile
 cat $infile | while read line 
 do
-  echo '['$j']' $line
+  echo '['$((i+1))']' $line
   src='"'$folder/$srclang/$line'"'
   des='"'$folder/$deslang/${outarr[${i}]}'"'
   cmd="python3 ./subtranslate/main.py --src-lang=$srclang --des-lang=$deslang --input-srt=$src --output-srt=$des"
   echo $cmd
   #eval $cmd
   i=$((i+1))
-  j=$((j+1))
 done
 
