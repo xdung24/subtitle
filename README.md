e898b2949302f04616a5f55a8052354652ada786
Dung Le <xdung24@gmail.com>
2022-11-29 20:21:09 +0700

    update scrpipt

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # subtitle
 This repo contains subtitle from many source and auto-translated
+```bash
+# sample
+bash autosub.sh moveies/giant-2010/
+```
--- autosub.sh
+++ autosub.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-folder="${PWD}/movies/giant-2010"
+#folder="${PWD}/movies/giant-2010"
+folder=${PWD}/$1
 infile="${folder}/in.txt"
-echo $folder
 outfile="${folder}/out.txt"
 
 srclang=en
@@ -14,11 +14,11 @@ readarray -t outarr < $outfile
 cat $infile | while read line 
 do
   echo '['$((i+1))']' $line
-  src='"'$folder/$srclang/$line'"'
-  des='"'$folder/$deslang/${outarr[${i}]}'"'
+  src='"'$folder$srclang/$line'"'
+  des='"'$folder$deslang/${outarr[${i}]}'"'
   cmd="python3 ./subtranslate/main.py --src-lang=$srclang --des-lang=$deslang --input-srt=$src --output-srt=$des"
   echo $cmd
-  #eval $cmd
+  eval $cmd
   i=$((i+1))
 done
 
